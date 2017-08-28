0f897654ae029f41b5d0d2efa52167497f880e46
Banyapon Poolsawas <banyapon.poo@dpu.ac.th>
2017-08-28 22:25:16 +0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,9 @@
-package com.daydev.qrcodeandroid;
+package com.daydev.qrcodesample;
 
 import android.content.Intent;
 import android.graphics.Point;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,22 +11,19 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
-import com.varvet.barcodereadersample.barcode.BarcodeCaptureActivity;
+
+import qrcode.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView QrResultText;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int BARCODE_READER_REQUEST_CODE = 1;
-
-    private TextView mResultTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mResultTextView = (TextView) findViewById(R.id.result_textview);
-
-        Button scanBarcodeButton = (Button) findViewById(R.id.scan_barcode_button);
+        QrResultText = (TextView) findViewById(R.id.result_textview);
+        Button scanBarcodeButton = (Button) findViewById(R.id.scan_qrcode_button);
         scanBarcodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == BARCODE_READER_REQUEST_CODE) {
@@ -43,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
                 if (data != null) {
                     Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
                     Point[] p = barcode.cornerPoints;
-                    mResultTextView.setText(barcode.displayValue);
-                } else mResultTextView.setText(R.string.no_barcode_captured);
+                    QrResultText.setText(barcode.displayValue);
+                } else QrResultText.setText(R.string.no_barcode_captured);
             } else Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
                     CommonStatusCodes.getStatusCodeString(resultCode)));
         } else super.onActivityResult(requestCode, resultCode, data);
